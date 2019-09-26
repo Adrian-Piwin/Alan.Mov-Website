abadd54259bc5c91426073ff709384aad473601f
Adrian <adrian7048@gmail.com>
2019-09-26 10:02:53 -0400

    Formatting and Portfolio button
    Changed the formatting, currently have sticky header, page grows with content, new buttons can now be fairly easily added.
    
    Todo: Content for portfolio
    
    Bugs: Hover effect does not work for second button after first button clicked

--- script.js
+++ script.js
@@ -9,40 +9,71 @@
       function init(){
         // the code to be called when the dom has loaded
         // #document has its nodes
-        var navOpt = document.getElementsByClassName("nav-opt");
-        var navName = document.getElementsByClassName("nav-name");
-        var navInfo = document.getElementById("info");
-        var loadOne = document.getElementsByClassName("load-one");
-        var loadTwo = document.getElementsByClassName("load-two");
-        var loadContainer = document.getElementsByClassName("nav-loader-container");
-        var navLine = document.getElementsByClassName("nav-line");
+        var navLoaderContainer = document.getElementsByClassName("nav-loader-container");
+        var navOpt = document.getElementsByClassName('nav-opt');
+
+        var infoContentLine = document.getElementById("info-content-line");
+        var infoWrap = document.getElementById("info-wrap");
+        var navInfo = document.getElementById("nav-info");
 
         // On click of info button
-        $(navInfo).one('click', function() {
-          $(navOpt).addClass('no-hover'); 
+        $(navOpt).one('click', function() {
+
+          $(this).addClass('no-hover'); 
 
           // Stop animation when finished
-          $(loadOne).one('animationiteration', function() {
+          $(this).children(".nav-loader-container").children(".load-one").one('animationiteration', function() {
             $(this).css("animation", "none");
           });
 
-          $(loadTwo).one('animationiteration', function() {
+          $(this).children(".nav-loader-container").children(".load-two").one('animationiteration', function() {
             $(this).css("animation", "none");
           });
 
           // Speed up
-          $(loadContainer).css("animation", "navSpinContainer ease-in-out 1.5s 1 0s");
+          $(this).children(".nav-loader-container").css("animation", "navSpinContainer ease-in-out 1.5s 1 0s");
 
           // Draw nav line when completed
-          $(navLine).css("animation", "drawLine ease-in-out 1.5s 1 1.5s");
-          $(navLine).css("animation-fill-mode", "forwards");
+          $(this).next().css({"animation":"drawLine ease-in-out 1.5s 1 1.5s", "animation-fill-mode":"forwards"});
+
+          // Reveal next option
+          $(this).next().next().show();
         });
       
-
         // On hover of button, reveal name
-        $(loadContainer).one('mouseover', function() {
-          $(navName).css("animation", "fadeIn 1s ease-in-out 0s 1 forwards");
+        $(navLoaderContainer).one('mouseenter', function() {
+          $(this).prev().css("animation", "fadeIn 1s ease-in-out 0s 1 forwards");
         });
+
+        $(navInfo).one('click', function() {
+          // Draw line to reveal content
+          drawContentLine(infoContentLine);
+          $(infoWrap).show();
+        });
+
+        function drawContentLine(canvasEle) {
+          var ctx = canvasEle.getContext("2d");
+          ctx.beginPath();
+          ctx.lineWidth = 6;
+          ctx.strokeStyle = "#0a7599";
+          ctx.moveTo(0, 3.5);
+          ctx.lineTo(canvasEle.width/6, 3.5);
+          ctx.stroke();
+
+          ctx.beginPath();
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = "#0a7599";
+          ctx.moveTo(canvasEle.width/6, 3.5);
+          ctx.lineTo(canvasEle.width/6+20, canvasEle.height - 3.5);
+          ctx.stroke();
+
+          ctx.beginPath();
+          ctx.lineWidth = 6;
+          ctx.strokeStyle = "#0a7599";
+          ctx.moveTo(canvasEle.width/6+20, canvasEle.height - 3.5);
+          ctx.lineTo(canvasEle.width-100, canvasEle.height - 3.5);
+          ctx.stroke();
+        }
       }
     })(window, document, undefined);
 
